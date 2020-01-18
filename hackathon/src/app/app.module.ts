b57b1c6fbe05076ec44e919c4236d8249c4ef291
Olivier <olileclerc7@gmail.com>
2020-01-18 14:38:18 -0500

    Can now access camera

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {WebcamModule} from 'ngx-webcam';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebcamModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
